8dde78e93f662ab8b475772aa9d5ab1c2e72037b
apidev234 <78847111+apidev234@users.noreply.github.com>
2021-11-17 20:49:16 +0530

    Um, Done.

---
+++ popupblocker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+TITLE="Logging Into Safari User"
+loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+TITLE="Changing Safari Setting"
+# Changing The Settings
+sudo -u $loggedInUser defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically 1
